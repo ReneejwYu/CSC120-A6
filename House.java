5fcbb502079f4a5fdcf0c8d5fe3c6150147d214e
Jiawei Yu <jyu06@smith.edu>
2024-11-01 23:58:33 -0400

    submission1

--- House.java
+++ House.java
@@ -1,12 +1,49 @@
 /* This is a stub for the House class */
-public class House {
 
-  public House() {
+import java.util.ArrayList;
+
+public class House extends Building {
+  private ArrayList<String> residents; // The <String> tells Java what kind of data we plan to store IN the ArrayList
+  private Boolean hasDiningRoom;
+
+  public House(String name, String address, int nFloors, Boolean hasDiningRoom) {
+    super(name, address, nFloors);
+    this.residents = new ArrayList<String>();
+    this.hasDiningRoom = hasDiningRoom;
     System.out.println("You have built a house: 🏠");
+  }
+  public boolean hasDiningRoom() {
+    return hasDiningRoom();
+  }
+
+  public int nResidents() {
+    return this.residents.size();
+  }
+
+  public void moveIn(String name) {
+    if (!residents.contains(name)){
+      residents.add(name);
+    } else {
+      System.out.println(name + " is already a resident of the house");
+    }
+  }
+
+  public String moveOut(String name) {
+    if (residents.contains(name)){
+      residents.remove(name);
+      return name;
+    } else {
+      System.out.println(name + " is not a resident of the house yet");
+      return null;
+    }
+  }
+
+  public boolean isResident(String person) {
+    return residents.contains(person);
   }
 
   public static void main(String[] args) {
-    new House();
+    House myHouse = new House("Ziskind House", "Henshaw", 3, true);
+    System.out.println(myHouse);
   }
-
 }
--- Library.java
+++ Library.java
@@ -1,12 +1,74 @@
 /* This is a stub for the Library class */
-public class Library {
+import java.util.Hashtable;
+import java.util.Set;
+public class Library extends Building {
+  private Hashtable<String, Boolean> collection;
 
-    public Library() {
+
+    public Library(String name, String address, int nFloors) {
+      super(name, address, nFloors);
+      this.collection = new Hashtable<String, Boolean>();
       System.out.println("You have built a library: 📖");
     }
-  
-    public static void main(String[] args) {
-      new Library();
+
+    public void addTitle(String title) {
+      if (!collection.containsKey(title)) {
+        collection.put(title, true);
+      } else {
+        System.out.println(title + " is already in the collection");
+      }
     }
-  
+
+    public String removeTitle(String title) {
+      if (collection.containsKey(title)) {
+        collection.remove(title);
+        return title;
+      } else {
+        System.out.println(title + " is not in the collection");
+        return null;
+      }
+    }
+
+    public void checkOut(String title) {
+      if (collection.containsKey(title) && collection.containsValue(true)) {
+        collection.put(title, false);
+      } else {
+        System.out.println(title+ "is not in the collection or already checked out.");
+      }
+    }
+
+    public void returnBook(String title) {
+      if (collection.containsKey(title) && collection.containsValue(false)) {
+        collection.put(title, true);
+      } else {
+        System.out.println(title+ "is not in the collection or already available.");
+      }
+    }
+
+    public boolean containsTitle(String title) {
+      return collection.containsKey(title);
+    }
+
+    public boolean isAvailable(String title) {
+      return collection.containsValue(true);
+    }
+
+    public void printCollection() {
+      System.out.println("Library Collection:");
+      for (String title : collection.keySet()) {
+        String status;
+        if (collection.get(title)) {
+          status = "Available";
+      } else {
+          status = "Checked Out";
+      }
+        System.out.println("Title: " + title);
+        System.out.println("Availability: " + status);
+      } 
+    }
+
+
+    public static void main(String[] args) {
+      Library myLibrary = new Library("Neilson Library", "1 chapin way", 4);
+    }
   }
